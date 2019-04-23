75cd1a9f1c1d38275ba76f9bcd4d3b8ef1a44784
Baobabstien <49429683+Baobabstien@users.noreply.github.com>
2019-04-23 22:55:05 +0200

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const Discord  = require('discord.js');
-const client = new Discord.Client();
-
-client.login("NTcwMzIwMDg2NDc3NDM5MDEx.XL9eeg.XFCquCQ8w_-Cdf6BV4ZD6cCGtNs");
-
-client.on('guildMemberAdd' , member =>{
-    member.guild.channels.get('304944083653361664').send('Bienvenue Muchacho! ' + member.user + 'Ravi de faire ta rencontre! :beer: ');
-    console.log('+1')
-})
